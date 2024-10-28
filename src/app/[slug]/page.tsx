a3ba73689474183a3385513c6cb41820e2302034
t1sev4 <tamikofficial@gmail.com>
2024-10-28 19:48:17 +0500

    add single page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import Add from '@/components/Add';
+import CustomizeProducts from '@/components/CustomizeProducts';
 import ProductImages from '@/components/ProductImages';
 import React from 'react';
 
@@ -11,7 +13,50 @@ const SinglePage = () => {
       </div>
 
       {/* text */}
-      <div className="w-full lg:w-1/2 flex flex-col gap-6">Texts</div>
+      <div className="w-full lg:w-1/2 flex flex-col gap-6">
+        <h1 className="text-4xl font-medium">Product Name</h1>
+        <p className="text-gray-500">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, animi
+          dolore. Placeat aspernatur nisi tempore aut ducimus vitae mollitia
+          nemo.
+        </p>
+        <div className="h-[2px] bg-gray-100" />
+        <div className="flex items-center gap-4">
+          <h3 className="text-xl text-gray-500 line-through">$59</h3>
+          <h2 className="font-medium text-2xl">$49</h2>
+        </div>
+        <div className="h-[2px] bg-gray-100" />
+        <CustomizeProducts />
+        <Add />
+        <div className="h-[2px] bg-gray-100" />
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
+            possimus quisquam modi dignissimos, deserunt sequi repellat,
+            doloribus dolorum est mollitia quo qui neque voluptatibus autem
+            ullam. Ipsum praesentium debitis vel.
+          </p>
+        </div>
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
+            possimus quisquam modi dignissimos, deserunt sequi repellat,
+            doloribus dolorum est mollitia quo qui neque voluptatibus autem
+            ullam. Ipsum praesentium debitis vel.
+          </p>
+        </div>
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
+            possimus quisquam modi dignissimos, deserunt sequi repellat,
+            doloribus dolorum est mollitia quo qui neque voluptatibus autem
+            ullam. Ipsum praesentium debitis vel.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
